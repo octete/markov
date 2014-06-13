f5cb425f93025c3c02298505909697de6409be3f
Jesus Roncero <jesus@we7.com>
2014-06-13 18:56:27 +0100

    updates.

--- markov.go
+++ markov.go
@@ -3,12 +3,40 @@
 // This is a small program to run the markov chain program
 // from the book The Practice of Programming.
 
+
 package main
 
 import (
 	"fmt"
+	//"string"
 )
+
+type suffix struct {
+	// a list
+}
+
+type state struct {
+	pref1 string
+	pref2 string
+	suf *suffix
+}
 
 func main() {
 	fmt.Println("Markov chains")
 }
+
+
+func lookup(){
+
+}
+
+func addSuffix(){
+
+
+
+}
+
+func add(){
+
+}
+
